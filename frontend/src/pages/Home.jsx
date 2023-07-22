a94b4f44c3008cd1056f29af9b46a2fa2375bd4e
thtshajjad <shagors@users.noreply.github.com>
2023-07-22 16:43:40 +0600

    design hero section and add images

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* ========== hero section ============= */}
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* ============ hero content ============ */}
+            <div className="">
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                  We help patients live a healthy, longer life.
+                </h1>
+                <p className="text__para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Impedit sint ex quidem officiis, esse quibusdam. Tenetur,
+                  fuga. Perferendis minima aliquam repudiandae, odit dolores in
+                  optio ea quasi voluptas quibusdam quas?
+                </p>
+
+                <button className="btn">Request an appoinment</button>
+              </div>
+
+              {/* ====== hero counter ========== */}
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of Experience</p>
+                </div>
+
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    10+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Clinic Location</p>
+                </div>
+
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Patient Satisfaction</p>
+                </div>
+              </div>
+            </div>
+
+            {/* ============ hero content ============ */}
+            <div className="flex gap-[30px] justify-end">
+              <div className="">
+                <img src={heroImg01} alt="Hero-Image" className="w-full" />
+              </div>
+              <div className="mt-[30px]">
+                <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                <img src={heroImg03} alt="" className="w-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
