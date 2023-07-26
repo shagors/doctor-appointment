1911e6fe167a611e946be4577b8b6a549131b285
thtshajjad <shagors@users.noreply.github.com>
2023-07-26 16:30:05 +0600

    some changes for footer not add it

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillGithub,
+  AiOutlineInstagram,
+  AiFillYoutube,
+} from "react-icons/ai";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Footer = () => {
   return <div>Footer</div>;
