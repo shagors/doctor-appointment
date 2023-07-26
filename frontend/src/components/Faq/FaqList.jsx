bab0e701c3e35e5f2de7127f0ac3847ed8b5d2a8
thtshajjad <shagors@users.noreply.github.com>
2023-07-26 15:08:08 +0600

    faq item pages design and components test

--- frontend/src/components/Faq/FaqList.jsx
+++ frontend/src/components/Faq/FaqList.jsx
@@ -1,7 +1,14 @@
 import { faqs } from "../../assets/data/faqs";
+import FaqItem from "./FaqItem";
 
 const FaqList = () => {
-  return <ul className="mt-[]">FaqList</ul>;
+  return (
+    <ul className="mt-[38px]">
+      {faqs.map((faq, index) => (
+        <FaqItem faq={faq} key={index} />
+      ))}
+    </ul>
+  );
 };
 
 export default FaqList;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,7 @@ import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -264,6 +265,11 @@ const Home = () => {
           <div className="flex justify-between gap-[50px] lg:gap-0">
             <div className="w-1/2 hidden md:block">
               <img src={faqImg} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">Most qustions by our beloved patients</h2>
+              <FaqList />
             </div>
           </div>
         </div>
